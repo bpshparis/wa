fba621d3ca21cbcde46813ec26309ddb96d3d934
bpshparis <iic_paris@fr.ibm.com>
2020-01-21 21:07:05 +0100

    init

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 JAVA_HOME=/opt/JDK/jre
 JAR_HOME=WebContent/WEB-INF/lib
 CLASSPATH=WebContent/WEB-INF/classes/
-MAIN_CLASS=com.bpshparis.wa.$1
+PACKAGE=com.bpshparis.wa
+
 
 for jar in $(ls $JAR_HOME/*.jar); do
   CLASSPATH="$CLASSPATH:$jar"
 done
 
-echo $CLASSPATH
+echo Run $@ ...
 
-$JAVA_HOME/bin/java -cp "$CLASSPATH" "$MAIN_CLASS" "$@"
+$JAVA_HOME/bin/java -cp "$CLASSPATH" "$PACKAGE"."$@"
 
